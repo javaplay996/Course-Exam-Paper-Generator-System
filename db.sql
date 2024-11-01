8d899854bd56c887905586d76f0a36bfd46bbdce
杨荧 <java996@artskyhome.com>
2024-11-01 15:42:46 +0800

    源码上传

---
+++ db.sql
@@ -0,0 +1,163 @@
+/*
+SQLyog Ultimate v11.3 (64 bit)
+MySQL - 5.7.32-log : Database - jspm2c325
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`jspm2c325` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `jspm2c325`;
+
+/*Table structure for table `config` */
+
+DROP TABLE IF EXISTS `config`;
+
+CREATE TABLE `config` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(100) NOT NULL COMMENT '配置参数名称',
+  `value` varchar(100) DEFAULT NULL COMMENT '配置参数值',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='配置文件';
+
+/*Data for the table `config` */
+
+insert  into `config`(`id`,`name`,`value`) values (1,'picture1','http://localhost:8080/jspm2c325/upload/picture1.jpg'),(2,'picture2','http://localhost:8080/jspm2c325/upload/picture2.jpg'),(3,'picture3','http://localhost:8080/jspm2c325/upload/picture3.jpg'),(6,'homepage',NULL);
+
+/*Table structure for table `exampaper` */
+
+DROP TABLE IF EXISTS `exampaper`;
+
+CREATE TABLE `exampaper` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `name` varchar(200) NOT NULL COMMENT '试卷名称',
+  `time` int(11) NOT NULL COMMENT '考试时长(分钟)',
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT '试卷状态',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1615622690283 DEFAULT CHARSET=utf8 COMMENT='试卷表';
+
+/*Data for the table `exampaper` */
+
+insert  into `exampaper`(`id`,`addtime`,`name`,`time`,`status`) values (1615622690282,'2021-03-13 16:04:49','考试1',60,1);
+
+/*Table structure for table `examquestion` */
+
+DROP TABLE IF EXISTS `examquestion`;
+
+CREATE TABLE `examquestion` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `paperid` bigint(20) NOT NULL COMMENT '所属试卷id（外键）',
+  `papername` varchar(200) NOT NULL COMMENT '试卷名称',
+  `questionname` varchar(200) NOT NULL COMMENT '试题名称',
+  `options` longtext COMMENT '选项，json字符串',
+  `score` bigint(20) DEFAULT '0' COMMENT '分值',
+  `answer` varchar(200) DEFAULT NULL COMMENT '正确答案',
+  `analysis` longtext COMMENT '答案解析',
+  `type` bigint(20) DEFAULT '0' COMMENT '试题类型，0：单选题 1：多选题 2：判断题 3：填空题（暂不考虑多项填空）',
+  `sequence` bigint(20) DEFAULT '100' COMMENT '试题排序，值越大排越前面',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1615622749406 DEFAULT CHARSET=utf8 COMMENT='试题表';
+
+/*Data for the table `examquestion` */
+
+insert  into `examquestion`(`id`,`addtime`,`paperid`,`papername`,`questionname`,`options`,`score`,`answer`,`analysis`,`type`,`sequence`) values (1615622724760,'2021-03-13 16:05:24',1615622690282,'考试1','1+1=？','[{\"text\":\"1\",\"code\":\"A\"},{\"text\":\"2\",\"code\":\"B\"},{\"text\":\"3\",\"code\":\"C\"},{\"text\":\"4\",\"code\":\"D\"}]',10,'B','1+1=2',0,1),(1615622749405,'2021-03-13 16:05:48',1615622690282,'考试1','2+2=?','[{\"text\":\"1\",\"code\":\"A\"},{\"text\":\"2\",\"code\":\"B\"},{\"text\":\"3\",\"code\":\"C\"},{\"text\":\"4\",\"code\":\"D\"}]',10,'D','2+2=4',0,2);
+
+/*Table structure for table `examrecord` */
+
+DROP TABLE IF EXISTS `examrecord`;
+
+CREATE TABLE `examrecord` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `userid` bigint(20) NOT NULL COMMENT '用户id',
+  `username` varchar(200) DEFAULT NULL COMMENT '用户名',
+  `paperid` bigint(20) NOT NULL COMMENT '试卷id（外键）',
+  `papername` varchar(200) NOT NULL COMMENT '试卷名称',
+  `questionid` bigint(20) NOT NULL COMMENT '试题id（外键）',
+  `questionname` varchar(200) NOT NULL COMMENT '试题名称',
+  `options` longtext COMMENT '选项，json字符串',
+  `score` bigint(20) DEFAULT '0' COMMENT '分值',
+  `answer` varchar(200) DEFAULT NULL COMMENT '正确答案',
+  `analysis` longtext COMMENT '答案解析',
+  `myscore` bigint(20) NOT NULL DEFAULT '0' COMMENT '试题得分',
+  `myanswer` varchar(200) DEFAULT NULL COMMENT '考生答案',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1615622777488 DEFAULT CHARSET=utf8 COMMENT='考试记录表';
+
+/*Data for the table `examrecord` */
+
+insert  into `examrecord`(`id`,`addtime`,`userid`,`username`,`paperid`,`papername`,`questionid`,`questionname`,`options`,`score`,`answer`,`analysis`,`myscore`,`myanswer`) values (1615622772830,'2021-03-13 16:06:11',1615622505575,NULL,1615622690282,'考试1',1615622724760,'1+1=？','[{\"text\":\"1\",\"code\":\"A\"},{\"text\":\"2\",\"code\":\"B\"},{\"text\":\"3\",\"code\":\"C\"},{\"text\":\"4\",\"code\":\"D\"}]',10,'B','1+1=2',0,'C'),(1615622777487,'2021-03-13 16:06:16',1615622505575,NULL,1615622690282,'考试1',1615622749405,'2+2=?','[{\"text\":\"1\",\"code\":\"A\"},{\"text\":\"2\",\"code\":\"B\"},{\"text\":\"3\",\"code\":\"C\"},{\"text\":\"4\",\"code\":\"D\"}]',10,'D','2+2=4',10,'D');
+
+/*Table structure for table `token` */
+
+DROP TABLE IF EXISTS `token`;
+
+CREATE TABLE `token` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `userid` bigint(20) NOT NULL COMMENT '用户id',
+  `username` varchar(100) NOT NULL COMMENT '用户名',
+  `tablename` varchar(100) DEFAULT NULL COMMENT '表名',
+  `role` varchar(100) DEFAULT NULL COMMENT '角色',
+  `token` varchar(200) NOT NULL COMMENT '密码',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '新增时间',
+  `expiratedtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '过期时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='token表';
+
+/*Data for the table `token` */
+
+insert  into `token`(`id`,`userid`,`username`,`tablename`,`role`,`token`,`addtime`,`expiratedtime`) values (1,1615622505575,'1','yonghu','用户','gb4znp9hqcenx7eeqe31wz8ueyrk09ak','2021-03-13 16:01:49','2021-03-13 17:05:56'),(2,1,'abo','users','管理员','q5t6t1pqkdxyzewb3a1a5e39bs95oo16','2021-03-13 16:02:25','2021-03-13 17:02:25');
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `username` varchar(100) NOT NULL COMMENT '用户名',
+  `password` varchar(100) NOT NULL COMMENT '密码',
+  `role` varchar(100) DEFAULT '管理员' COMMENT '角色',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '新增时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`username`,`password`,`role`,`addtime`) values (1,'abo','abo','管理员','2021-03-13 15:52:21');
+
+/*Table structure for table `yonghu` */
+
+DROP TABLE IF EXISTS `yonghu`;
+
+CREATE TABLE `yonghu` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `addtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gerenzhanghao` varchar(200) NOT NULL COMMENT '个人账号',
+  `mima` varchar(200) NOT NULL COMMENT '密码',
+  `xingming` varchar(200) NOT NULL COMMENT '姓名',
+  `xingbie` varchar(200) DEFAULT NULL COMMENT '性别',
+  `nianling` int(11) DEFAULT NULL COMMENT '年龄',
+  `shenfenzhenghaoma` varchar(200) DEFAULT NULL COMMENT '身份证号码',
+  `shoujihaoma` varchar(200) DEFAULT NULL COMMENT '手机号码',
+  `xiangpian` varchar(200) DEFAULT NULL COMMENT '相片',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `gerenzhanghao` (`gerenzhanghao`)
+) ENGINE=InnoDB AUTO_INCREMENT=1615622505576 DEFAULT CHARSET=utf8 COMMENT='用户';
+
+/*Data for the table `yonghu` */
+
+insert  into `yonghu`(`id`,`addtime`,`gerenzhanghao`,`mima`,`xingming`,`xingbie`,`nianling`,`shenfenzhenghaoma`,`shoujihaoma`,`xiangpian`) values (11,'2021-03-13 15:52:21','用户1','123456','姓名1','男',1,'440300199101010001','13823888881','http://localhost:8080/jspm2c325/upload/yonghu_xiangpian1.jpg'),(12,'2021-03-13 15:52:21','用户2','123456','姓名2','男',20,'440300199202020002','13823888882','http://localhost:8080/jspm2c325/upload/yonghu_xiangpian2.jpg'),(14,'2021-03-13 15:52:21','用户4','123456','姓名4','男',4,'440300199404040004','13823888884','http://localhost:8080/jspm2c325/upload/yonghu_xiangpian4.jpg'),(15,'2021-03-13 15:52:21','用户5','123456','姓名5','男',5,'440300199505050005','13823888885','http://localhost:8080/jspm2c325/upload/yonghu_xiangpian5.jpg'),(16,'2021-03-13 15:52:21','用户6','123456','姓名6','男',6,'440300199606060006','13823888886','http://localhost:8080/jspm2c325/upload/yonghu_xiangpian6.jpg'),(1615622505575,'2021-03-13 16:01:45','1','1','展示','男',18,'552632221411152632','13466623632','http://localhost:8080/jspm2c325/upload/1615622529231.png');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
